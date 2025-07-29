e5a9e967c266386178cc4ca2bcb90605c3e6886f
jacobg1 <greenwald.j8@gmail.com>
2025-07-29 16:15:49 -0400

    add project pipeline

--- lib/project-pipelines-stack.ts
+++ lib/project-pipelines-stack.ts
@@ -1,16 +1,17 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { CodePipeline, CodePipelineSource, ShellStep } from "aws-cdk-lib/pipelines";
+import { Construct } from "constructs";
 
 export class ProjectPipelinesStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'ProjectPipelinesQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const pipeline = new CodePipeline(this, "ProjectPipelines", {
+      pipelineName: "ProjectPipelines",
+      synth: new ShellStep("Synth", {
+        input: CodePipelineSource.gitHub("jacobg1/project-pipelines", "main"),
+        commands: ["npm ci", "npm run build", "npx cdk synth"],
+      }),
+    });
   }
 }
