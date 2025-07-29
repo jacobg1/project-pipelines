5b0dadadcca70d45936d23a4d5e630ad61bdd744
jacobg1 <greenwald.j8@gmail.com>
2025-07-29 19:29:58 -0400

    add new pipeline

--- lib/project-pipelines-stack.ts
+++ lib/project-pipelines-stack.ts
@@ -1,29 +1,30 @@
 import * as cdk from "aws-cdk-lib";
-import * as ssm from "aws-cdk-lib/aws-ssm";
 
-import { CodePipeline, CodePipelineSource, ShellStep } from "aws-cdk-lib/pipelines";
 import { Construct } from "constructs";
+import { getSSMParam, createPipeline } from "../src";
 
 export class ProjectPipelinesStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const codeConnectionArn = ssm.StringParameter.valueForStringParameter(
+    const connectionArn = getSSMParam(this, "/project-pipeline/code-connection");
+
+    createPipeline(
       this,
-      "/project-pipeline/code-connection"
+      "ProjectPipelines",
+      {
+        name: "jacobg1/project-pipelines",
+        branch: "main",
+        connectionArn,
+      },
+      ["npm ci", "npm run build", "npx cdk synth"]
     );
 
-    console.log("ENV", process.env.CDK_DEFAULT_ACCOUNT);
-    console.log("ENV", process.env.CDK_DEFAULT_REGION);
-
-    new CodePipeline(this, "ProjectPipelines", {
-      pipelineName: "ProjectPipelines",
-      synth: new ShellStep("Synth", {
-        input: CodePipelineSource.connection("jacobg1/project-pipelines", "main", {
-          connectionArn: codeConnectionArn,
-        }),
-        commands: ["npm ci", "npm run build", "npx cdk synth"],
-      }),
-    });
+    const spaceSearchPipeline = createPipeline(
+      this,
+      "SpaceSearchPipeline",
+      { name: "jacobg1/NasaSearch", branch: "pipeline-test", connectionArn },
+      ["npm ci"]
+    );
   }
 }
