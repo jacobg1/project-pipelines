87fa03fefdc2566cb90f87f6d50436245402f184
jacobg1 <greenwald.j8@gmail.com>
2025-07-29 17:44:20 -0400

    get connection string from ssm

--- lib/project-pipelines-stack.ts
+++ lib/project-pipelines-stack.ts
@@ -1,4 +1,6 @@
 import * as cdk from "aws-cdk-lib";
+import * as ssm from "aws-cdk-lib/aws-ssm";
+
 import { CodePipeline, CodePipelineSource, ShellStep } from "aws-cdk-lib/pipelines";
 import { Construct } from "constructs";
 
@@ -6,11 +8,18 @@ export class ProjectPipelinesStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const codeConnectionArn = ssm.StringParameter.valueForStringParameter(
+      this,
+      "/project-pipeline/code-connection"
+    );
+
+    console.log("codeConnectionArn", codeConnectionArn);
+
     const pipeline = new CodePipeline(this, "ProjectPipelines", {
       pipelineName: "ProjectPipelines",
       synth: new ShellStep("Synth", {
         input: CodePipelineSource.connection("jacobg1/project-pipelines", "main", {
-          connectionArn: process.env.CDK_CODEBUILD_CONNECTION_ARN as string,
+          connectionArn: codeConnectionArn,
         }),
         commands: ["npm ci", "npm run build", "npx cdk synth"],
       }),
