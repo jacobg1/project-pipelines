387b52b17ee8d7a07fc79670c85bb7b9e41da53d
jacobg1 <greenwald.j8@gmail.com>
2025-07-30 14:54:11 -0400

    remove unused function

--- src/ssm.ts
+++ src/ssm.ts
@@ -1,13 +1,7 @@
 import { StringParameter } from "aws-cdk-lib/aws-ssm";
 import { ProjectPipelinesStack } from "../lib/project-pipelines-stack";
-import { SecretValue } from "aws-cdk-lib";
 
 export function getSSMParam(stack: ProjectPipelinesStack, name: string): string {
   const param = StringParameter.valueForStringParameter(stack, name);
-  return param;
-}
-
-export function getSecureSSMParam(name: string): SecretValue {
-  const param = SecretValue.ssmSecure(name);
   return param;
 }
