d8986a52191ccfe090b367b70f0d349da7358d10
jacobg1 <greenwald.j8@gmail.com>
2025-07-30 14:55:57 -0400

    create pipeline for serverless deployments

--- lib/project-pipelines-stack.ts
+++ lib/project-pipelines-stack.ts
@@ -1,16 +1,17 @@
 import * as cdk from "aws-cdk-lib";
 
 import { Construct } from "constructs";
-import { getSSMParam, getSecureSSMParam, createCDKPipeline, createPipeline } from "../src";
+import { getSSMParam, createCodePipeline, createPipeline } from "../src";
+import { createBuildRole } from "../src/iam";
 
 export class ProjectPipelinesStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const connectionArn = getSSMParam(this, "/project-pipeline/code-connection");
-    const serverlessLoginKey = getSecureSSMParam("/serverless/login/key");
+    const role = createBuildRole(this, "ProjectPipelinesRole");
 
-    createCDKPipeline(
+    createCodePipeline(
       this,
       "ProjectPipelines",
       {
@@ -21,16 +22,11 @@ export class ProjectPipelinesStack extends cdk.Stack {
       ["npm ci", "npm run build", "npx cdk synth"]
     );
 
-    createPipeline(
-      this,
-      "SpaceSearchPipeline",
-      {
-        name: "NasaSearch",
-        owner: "jacobg1",
-        branch: "pipeline-test",
-        connectionArn,
-      },
-      serverlessLoginKey
-    );
+    createPipeline(this, "SpaceSearchPipeline", role, {
+      name: "NasaSearch",
+      owner: "jacobg1",
+      branch: "pipeline-test",
+      connectionArn,
+    });
   }
 }
